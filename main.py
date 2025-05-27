0b19f909ccc455400a29c54d72566954d93a9217
Binwin6724 <binwinviju225096@gmail.com>
2025-05-27 17:18:30 +0530

    added

--- main.py
+++ main.py
@@ -352,4 +352,4 @@ def serve_pdf(filename):
     return send_from_directory('uploads', filename)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", debug=True)
